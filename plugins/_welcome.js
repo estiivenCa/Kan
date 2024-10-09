80846e7f41cffc7e8ec95353620770e6c8d114aa
estiivenCa <duvanelroloxd@gmail.com>
2024-10-09 08:15:11 -0500

    push8

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -4,61 +4,60 @@ import fetch from 'node-fetch';
 export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return true;
 
-  let vn = 'https://qu.ax/cTDa.mp3';
-  let vn2 = 'https://qu.ax/xynz.mp3';
   let welc = welcome;
   let adi = adios;
+   let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://tinyurl.com/ylgu47w3')
+  let usuario = `@${m.sender.split`@`[0]}`
   let chat = global.db.data.chats[m.chat];
   const getMentionedJid = () => {
     return m.messageStubParameters.map(param => `${param}@s.whatsapp.net`);
   };
 
   let who = m.messageStubParameters[0] + '@s.whatsapp.net';
-  let user = global.db.data.users[who];
+  let userName;
 
-  let userName = user ? user.name : await conn.getName(who);
+  try {
+    userName = global.db.data.users[who]?.name || await conn.getName(who);
+  } catch (error) {
+    console.error('Error al obtener el nombre del usuario:', error);
+    userName = 'Usuario'; // Nombre por defecto en caso de error
+  }
 
   if (chat.welcome && m.messageStubType === 27) {
     this.sendMessage(m.chat, {
-      audio: { url: vn },
+      text: `╭══•🔥ೋ•๑♡๑•ೋ🔥•══╮\n¡Bienvenido/a,  @${m.messageStubParameters[0].split`@`[0]}!\n╰══•🔥 ೋ•๑♡๑•ೋ 🔥•══╯\n\nEsperamos que disfrutes tu estancia en el grupo.\n*_Recuerda leer la descripción_*\n🥀*ੈ✩‧₊˚༺☆༻*ੈ✩˚🍁`,
       contextInfo: {
         mentionedJid: getMentionedJid(),
         "externalAdReply": {
           "thumbnail": welc,
           "title": "  ͟͞ Ｗ Ｅ Ｌ Ｃ Ｏ Ｍ Ｅ ͟͞  ",
-          "body": `${userName}!`,
+          "body": ` KanBot Best - Bot`,
           "previewType": "PHOTO",
           "thumbnailUrl": null,
           "showAdAttribution": true,
           sourceUrl: [yt, md, channel].sort(() => 0.5 - Math.random())[0]
         }
-      },
-      ptt: true,
-      mimetype: 'audio/mpeg',
-      fileName: 'welcome.mp3'
+      }
     }, { quoted: fkontak });
   }
 
   if (chat.welcome && (m.messageStubType === 28 || m.messageStubType === 32)) {
     this.sendMessage(m.chat, {
-      audio: { url: vn2 },
+      text: `╭══•🔥ೋ•๑♡๑•ೋ🔥•══╮\n¡Adiós, @${m.messageStubParameters[0].split`@`[0]}!\n╰══•🔥 ೋ•๑♡๑•ೋ 🔥•══╯\n\nGracias por haber estado con nosotros.\n🥀*ੈ✩‧₊˚༺☆༻*ੈ✩˚🍁`,
       contextInfo: {
         mentionedJid: getMentionedJid(),
         "externalAdReply": {
-        "thumbnail": adi,
-        "title": '  ͟͞ Ａ Ｄ Ｉ Ｏ Ｓ ͟͞  ',
-        "body": `${userName}, se despide.`,
-        "previewType": "PHOTO",
+          "thumbnail": adi,
+          "title": '  ͟͞ Ａ Ｄ Ｉ Ｏ Ｓ ͟͞  ',
+          "body": `${userName}, se despide.`,
+          "previewType": "PHOTO",
           "showAdAttribution": true,
           "containsAutoReply": true,
-         "thumbnailUrl": null,
+          "thumbnailUrl": null,
           "showAdAttribution": true,
           "sourceUrl": redes
         }
-      },
-      ptt: true,
-      mimetype: 'audio/mpeg',
-      fileName: 'bye.mp3'
+      }
     }, { quoted: fkontak });
   }
 }
