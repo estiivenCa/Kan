9b81bd58e193937615fca222dfb197e1621598c5
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-10-09 08:48:03 -0500

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,4 +1,4 @@
-let media = './src/Grupo.jpg'
+/* let media = './src/Grupo.jpg'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
     await conn.sendMessage(m.chat, { react: { text: '⚡️', key: m.key } })
@@ -31,4 +31,4 @@ handler.command = ['grupos','linksk','gruposofc','gruposoficiales']
 handler.register = true
 handler.exp = 33
 
-export default handler
+export default handler */
