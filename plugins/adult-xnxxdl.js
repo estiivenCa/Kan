08526b1aac6c9832c47b8b80a1b53e54a5890477
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-10-09 08:32:20 -0500

    Update adult-xnxxdl.js

--- plugins/adult-xnxxdl.js
+++ plugins/adult-xnxxdl.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+/* import fetch from 'node-fetch';
 import cheerio from 'cheerio';
 
 
@@ -67,3 +67,4 @@ async function xnxxdl(URL) {
     }).catch((err) => reject({code: 503, status: false, result: err}));
   });
 }
+ */
