3af6f3aabaf5698060ae85ebf98c01d541516bf0
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-10-09 08:54:52 -0500

    Update sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -21,6 +21,5 @@ const handler = async (m, {conn, text}) => {
 handler.help = ['wm <packname>|<author>'];
 handler.tags = ['sticker'];
 handler.group = true;
-handler.register = true
 handler.command = ['take','wm'];
 export default handler;
