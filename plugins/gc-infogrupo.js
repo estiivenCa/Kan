b88ea841b2947df45578a93f7b69a12972b479f6
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-10-09 09:20:13 -0500

    Update gc-infogrupo.js

--- plugins/gc-infogrupo.js
+++ plugins/gc-infogrupo.js
@@ -38,6 +38,5 @@ ${listAdmin}
 handler.help = ['infogrupo'];
 handler.tags = ['grupo'];
 handler.command = ['infogrupo', 'gp'];
-handler.register = true
 handler.group = true;
 export default handler;
